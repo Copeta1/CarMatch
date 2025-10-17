5f10b17432d563fd583a727c3d0a879a66f1994e
Copeta1 <pecolajrobert1@gmail.com>
2025-10-17 18:02:36 +0200

    feat: added header for brands an icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,22 @@
+import { router } from "expo-router";
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import SearchBar from "../../components/SearchBar";
 import { icons } from "../../constants/icons";
 
 export default function Index() {
+  const brands = [
+    { name: "BMW", icon: require("../../assets/icons/car_brands/BMW.png") },
+    {
+      name: "VW",
+      icon: require("../../assets/icons/car_brands/VW.png"),
+    },
+    {
+      name: "Mercedes",
+      icon: require("../../assets/icons/car_brands/mercedes.png"),
+    },
+    { name: "Tesla", icon: require("../../assets/icons/car_brands/tesla.png") },
+  ];
+
   return (
     <View className="flex-1 bg-white">
       <View className="bg-primary w-full h-64 rounded-b-3xl shadow-md">
@@ -39,7 +53,39 @@ export default function Index() {
           </View>
         </View>
       </View>
-      <ScrollView className="flex-1 px-5"></ScrollView>
+      <ScrollView className="flex-1 px-8 py-8">
+        <View className="flex-row justify-between items-center">
+          <View>
+            <Text className="font-bold text-2xl">Brands</Text>
+          </View>
+          <TouchableOpacity>
+            <Text className="text-blue-500" onPress={() => router.push("/")}>
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {/* Brands of cars*/}
+        <View className="flex-row flex-wrap justify-between py-4">
+          {brands.map((brand, index) => (
+            <TouchableOpacity
+              key={index}
+              className="items-center mb-6 w-1/5"
+              onPress={() => console.log(`Selected ${brand.name}`)}
+            >
+              <View className="bg-gray-100 w-16 h-16 rounded-full justify-center items-center">
+                <Image
+                  source={brand.icon}
+                  className="w-8 h-8"
+                  resizeMode="contain"
+                />
+              </View>
+              <Text className="mt-2 text-center font-medium text-gray-700">
+                {brand.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
